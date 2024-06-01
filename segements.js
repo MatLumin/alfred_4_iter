57dab410b98e1dbe28b69c0267f578f9c5adfec7
= <88319573+MatLumin@users.noreply.github.com>
2024-06-01 20:06:02 +0330

    replace segment.js with correct segments's attrs

--- segements.js
+++ segements.js
@@ -20,16 +20,20 @@ from_singular_seg(
 	
 	
 	
+	
+	
 
 
 		<h1 id="unix_title" onclick="pbv()"> UNIX: </h1>
 	<div id="hour_minute_flex">
-		<h1 id="hour_dispaly"></h1>
-		<pre id="hour_minute_delimiter">:</pre>
-		<h1 id="minute_display"></h1>    
+		<h1 id="hour_dispaly">18</h1>
+		<pre id="hour_minute_delimiter"> </pre>
+		<h1 id="minute_display">30</h1>    
 	</div>
-	<h2 id="unix_clock">1715599131885</h2>
-	<h2 id="full_date">5/13/2024</h2>		
+	<h2 id="unix_clock">1716908408</h2>
+	<h2 id="full_date">5/28/2024</h2>		
+	
+	
 	
 	
 	
@@ -59,14 +63,18 @@ from_singular_seg(
 	
 	
 	
+	
+	
 			
 	
 	
 	<h1 class="weather_label" onclick="refresh_the_page()">Weather</h1>
-	<img id="weather_status_icon"></img> 
+	<img id="weather_status_icon" src="/static/weather_icons/01d.png"> 
 	<h1 id="weather_title_holder"></h1> 
-	<h1 id="weather_caption_holder"></h1> 
+	<h1 id="weather_caption_holder">clear sky</h1> 
 	 
+	
+	
 	
 	
 	
@@ -93,22 +101,24 @@ from_singular_seg(
 	
 	
 	
+	
+	
 				
 
 	<h2 class="weather_label">Speed:</h2>
-		<h2 class="weather_value" id="wind_speed_holder"></h2>
+		<h2 class="weather_value" id="wind_speed_holder">5.14 km/h</h2>
 
-	<h2 class="weather_label" hidden>angle:</h2>
-		<h2 class="weather_value" id="wind_angle_holder" hidden></h2>
+	<h2 class="weather_label" hidden="">angle:</h2>
+		<h2 class="weather_value" id="wind_angle_holder" hidden="">310 deg</h2>
 		
 		
-	<canvas id="air_speed_angle_canvas" height="47px" width="61px"></canvas>
-	<h2 class="weather_label" hidden>gust:</h2>
-		<h2 class="weather_value" id="wind_gust_holder" hidden></h2>		
+	<canvas id="air_speed_angle_canvas" height="47" width="61"></canvas>
+	<h2 class="weather_label" hidden="">gust:</h2>
+		<h2 class="weather_value" id="wind_gust_holder" hidden="">undefined</h2>		
 	
-	<img class="wind_logo" src="static/wind_logo.png">
-	<img class="wind_logo" src="static/wind_logo.png">	
-	<img class="wind_logo" src="static/wind_logo.png">	
+	<img class="wind_logo" src="static/wind_logo.png" style="animation-duration: 2.91829s; animation-delay: 2.55825s;">
+	<img class="wind_logo" src="static/wind_logo.png" style="animation-duration: 2.91829s; animation-delay: 1.91285s;">	
+	<img class="wind_logo" src="static/wind_logo.png" style="animation-duration: 2.91829s; animation-delay: 2.18492s;">	
 	
 	
 	
@@ -119,6 +129,8 @@ from_singular_seg(
 			
 
 			
+	
+	
 	
 	
 	
@@ -143,7 +155,7 @@ from_singular_seg(
 
 from_singular_seg(
 	"4",
-	566,436,336,115,
+	568,370,369,115,
 	`
 	
 	
@@ -161,18 +173,20 @@ from_singular_seg(
 	
 	
 	
-	<img class="qr_code_holder" src="static/app_download_qr_code.png">
-
-	</img>
-
-	
-	
-	
 	
 	
 	
 
+	
+	
+	
+	
+	
+	
 
+
+	
+	
 	
 	
 	
@@ -184,12 +198,16 @@ from_singular_seg(
 
 from_singular_seg(
 	"5",
-	787,0,96,122,
+	791,0,96,122,
 	`
 	
-	<img src="/static/mossito_icon_1.PNG" id="mositto_logo" >
+	
+	
+	<img src="/static/mossito_icon_1.PNG" id="mositto_logo">
 
 			
+	
+	
 	
 	
 	`
@@ -197,7 +215,7 @@ from_singular_seg(
 
 from_singular_seg(
 	"6",
-	794,94,152,229,
+	797,90,135,229,
 	`
 	
 	
@@ -218,9 +236,11 @@ from_singular_seg(
 	
 	
 	
+	
+	
 			
 			
-	<video id="first_video_displayer" class="filling_video" src="/static/0.mp4" loop>
+	<video id="first_video_displayer" class="filling_video" src="/static/0.mp4" loop="">
 
 	</video>
 	
@@ -233,10 +253,10 @@ from_singular_seg(
 	
 	
 	
+	
+	
 	`
 	);
-
-
 
 from_singular_seg(
 	"8",
@@ -256,6 +276,14 @@ from_singular_seg(
 	
 	
 	
+		
+	
+	<img class="qr_code_holder" src="static/app_download_qr_code.png">
+
+	</img>
+
+	
+	
 	
 
 	
@@ -268,6 +296,8 @@ from_singular_seg(
 
 
 
+	
+	
 	
 	
 	
@@ -301,9 +331,11 @@ from_singular_seg(
 	
 	
 	
+	
+	
 	<h1 id="net_speed_title">Net speed:</h1>	
 	<canvas id="net_speed_gauge" width="100" height="50"></canvas>
-	<h1 id="net_speed_value"></h1>
+	<h1 id="net_speed_value">3.05MB</h1>
 	meow
 	
 	
@@ -323,13 +355,17 @@ from_singular_seg(
 	
 	
 	
+	
+	
 	`
 	);
 
 from_singular_seg(
 	"10",
-	452,95,289,343,
+	454,90,269,343,
 	`
+	
+	
 	
 	
 	
@@ -344,7 +380,7 @@ from_singular_seg(
 	
 
 			
-	<video id="second_video_displayer" class="filling_video" src="/static/0.mp4" loop>
+	<video id="second_video_displayer" class="filling_video" src="/static/0.mp4" loop="">
 
 	</video>
 	
@@ -358,13 +394,17 @@ from_singular_seg(
 	
 	
 	
+	
+	
 	`
 	);
 
 from_singular_seg(
 	"11",
-	341,0,338,113,
+	343,0,338,113,
 	`
+	
+	
 	
 	
 	
@@ -372,59 +412,13 @@ from_singular_seg(
 	
 	
 	<h1 class="weather_label">Temp:</h1>
-	<canvas id="temp_canvas" height="47px" width="61px"></canvas>
-	<h2 class="weather_value" id="temp_holder"></h2>
+	<canvas id="temp_canvas" height="47" width="61"></canvas>
+	<h2 class="weather_value" id="temp_holder">31C</h2>
 
 
 	<h1 class="weather_label">humidity:</h1>
-	<canvas id="humidity_canvas" height="47px" width="61px"></canvas>	
-	<h2 class="weather_value" id="humidity_holder"></h2>
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	`
-	);
-
-from_singular_seg(
-	"11",
-	908,240,192,116,
-	`
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-	
-			
-	<img src="/static/abaas_abad_company_logo.png" class="main_logo" id="authority_logo_holder">
-	
-			
-	
-	
+	<canvas id="humidity_canvas" height="47" width="61"></canvas>	
+	<h2 class="weather_value" id="humidity_holder">8 g/m**-3</h2>
 	
 	
 	
@@ -446,8 +440,62 @@ from_singular_seg(
 
 from_singular_seg(
 	"11",
-	453,436,384,115,
+	908,225,192,116,
 	`
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+			
+	<img src="/static/gov_icon.png" class="main_logo" id="authority_logo_holder">
+	
+			
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	
+	`
+	);
+
+from_singular_seg(
+	"11",
+	455,407,384,115,
+	`
+	
+	
 	
 
 
@@ -457,17 +505,17 @@ from_singular_seg(
 	
 	<h1 class="air_polltion_polluter_title">NO2</h1>
 	<span class="air_pollution_value" id="value_holder_of_no2">
-	<canvas id="canvas_of_no2" height="47px" width="61px" ></canvas>
+	<canvas id="canvas_of_no2" height="47" width="61"></canvas>
 	
 	
 	<h1 class="air_polltion_polluter_title">O3</h1>
 	<span class="air_pollution_value" id="value_holder_of_o3">
-	<canvas id="canvas_of_o3" height="47px" width="61px" ></canvas>
+	<canvas id="canvas_of_o3" height="47" width="61"></canvas>
 	
 	
 	<h1 class="air_polltion_polluter_title">CO</h1>
 	<span class="air_pollution_value" id="value_holder_of_co">
-	<canvas id="canvas_of_co" height="47px" width="61px" ></canvas>
+	<canvas id="canvas_of_co" height="47" width="61"></canvas>
 
 
 
@@ -479,38 +527,44 @@ from_singular_seg(
 
 	
 	
+	
+	</span></span></span>
 	
 	`
 	);
 
 from_singular_seg(
 	"12",
-	681,384,430,114,
+	683,359,430,114,
 	`
+	
+	
 	
 	<h1 class="air_polltion_title">AIR POLLUTOIN</h1>
 	
 
 	<h1 class="air_polltion_polluter_title">SO2</h1>
 	<span class="air_pollution_value" id="value_holder_of_so2">	
-	<canvas id="canvas_of_so2" height="47px" width="61px" ></canvas>
+	<canvas id="canvas_of_so2" height="47" width="61"></canvas>
 	
 
 	
 	<h1 class="air_polltion_polluter_title">PM2.5</h1>	
 	<span class="air_pollution_value" id="value_holder_of_pm2_5">	
-	<canvas id="canvas_of_pm2_5" height="47px" width="61px" ></canvas>
+	<canvas id="canvas_of_pm2_5" height="47" width="61"></canvas>
 	
 	
 		
 	<h1 class="air_polltion_polluter_title">PM10</h1>
 	<span class="air_pollution_value" id="value_holder_of_pm10">
-	<canvas id="canvas_of_pm10" height="47px" width="61px" ></canvas>
+	<canvas id="canvas_of_pm10" height="47" width="61"></canvas>
 	
 	
 		
 	<h1 class="air_polltion_polluter_title">NO</h1>
 	<span class="air_pollution_value" id="value_holder_of_no">
-	<canvas id="canvas_of_no" height="47px" width="61px" ></canvas>	
+	<canvas id="canvas_of_no" height="47" width="61"></canvas>	
+	</span></span></span></span>
+	
 	`
 	);
